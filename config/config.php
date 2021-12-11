b760a958c2cbc9101c5445669b0f4d582bdfa6f1
ampan0907 <1801010814fx@cyber-u.ac.jp>
2021-12-11 23:15:05 +0900

    初期コミット

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * 設定ファイルはconfigディレクトリの中に入れてください
+ * 
+ * 使い方: このファイルをrequire(もしくはinclude)して変数に代入する。
+ */
+return array(
+    'db'     => 'mysql',
+    'dbname' => 'babe-base',
+);
---
+++ tests/SampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * PHPのテストを書く場合はtestsディレクトリの中に入れてください。
+ */
+class SampleTest
+{
+}
