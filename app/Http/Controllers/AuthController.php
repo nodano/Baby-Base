2bb2714080b7b33275804e0e2da6e43844331f9c
Ikema19 <masa0208yuki@icloud.com>
2021-12-23 22:21:19 +0900

    認証機能の入力チェック 仮コミット #24

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,6 +37,38 @@ class AuthController extends Controller
     // TODO: 文字の形式
     // TODO: パスワードの一致
 
+    // TODO: 空文字 
+    // 空白の削除
+    $hoge = trim($hoge);
+    // 文字列が格納されているか
+    if (empty($hoge)) {
+      // 文字列が格納されていない場合
+      $this->push("auth/signup");
+    }
+
+
+    //TODO: 文字の形式
+    //文字数は足りているか
+    if (mb_strlen($hoge) < 8) {
+      //8文字未満の場合の処理
+    }
+    // 英数字であるかチェック
+    if (preg_match('/^[a-zA-Z0-9]+$/', $hoge)) {
+      // 英数字の場合
+    } else {
+      // 英数字ではない場合
+      $this->push("auth/signup");
+    }
+
+
+    // TODO: パスワードの一致
+    if ($password == $pass) {
+      // パスワードが一致していた場合
+    } else {
+      // パスワードの不一致
+      $this->push("auth/signup");
+    }
+
     // ミスがあれば 戻る
     if (false) {
       $this->push("auth/signup");
@@ -58,6 +90,47 @@ class AuthController extends Controller
     // 入力値の確認
     // TODO: 空文字
     // TODO: 文字の形式
+
+    // TODO: 空文字 
+    // 空白の削除
+    $hoge = trim($hoge);
+    // 文字列が格納されているか
+    if (empty($hoge)) {
+      // 文字列が格納されていない場合
+      $this->push("auth/signup");
+    }
+
+
+    //TODO: 文字の形式
+    //文字数は足りているか
+    if (mb_strlen($hoge) < 8) {
+      //8文字未満の場合の処理
+    }
+
+    // 英数字であるかチェック
+    if (preg_match('/^[a-zA-Z0-9]+$/', $hoge)) {
+      // 英数字の場合
+    }
+    // 日本語であるかチェック
+    if (!preg_match("/^[ぁ-んァ-ヶ一-龠々]+$/u", $hoge)) {
+      // 日本語の場合
+    }
+
+    //Eメールの場合
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      // Eメールの場合
+
+    }
+
+
+    //TODO: パスワードの一致
+    if ($hoge1 == $hoge2) {
+      // パスワードが一致していた場合
+    } else {
+      // パスワードの不一致
+      $this->push("auth/signup");
+    }
+
 
     // ミスがあれば 戻る
     if (false) {
@@ -101,6 +174,18 @@ class AuthController extends Controller
     }
     // 入力値の確認
     // TODO: 空文字
+
+    // TODO: 空文字 
+    // 空白の削除
+    $hoge = trim($hoge);
+
+
+    //TODO: 文字の形式
+    // 文字列が格納されているか
+    if (empty($hoge)) {
+      // 文字列が格納されていない場合
+      $this->push("auth/signup");
+    }
 
     // ミスがあれば 戻る
     if (false) {
