6c8c4fc4fb95c04166ed5f323ddbb2dbc778c188
ritsuki990907 <ritsuki990907@gmail.com>
2022-02-17 18:39:53 +0900

    売上画面簡易デザイン

--- resources/views/user/profit.php
+++ resources/views/user/profit.php
@@ -1,4 +1,13 @@
 <h1>売上確認</h1>
-<pre>
-  <?php var_dump($transfers); ?>
-</pre>
+<div class="profit">
+  <ul>
+    <?php foreach ($transfers as $transfer) : ?>
+      <li class="profit-item">
+        <span><?php echo $transfer['name']; ?></span>
+        <span><?php echo $transfer['amount']; ?></span>
+      </li>
+    <?php endforeach; ?>
+
+    <li class="profit-item profit-total"><span>合計: </span><span><?php echo $total; ?></span></li>
+  </ul>
+</div>
